4e6ef4da0961a9458446bf2a4404aa2a0ad9f9dc
rohith4413 <dokkuvenkatrohith@gmail.com>
2026-03-02 09:28:29 +0530

    Init commit

---
+++ python/core/Higher order function/higher function 1.py
@@ -0,0 +1,30 @@
+#1
+# l=[[1,2],[3,4],[5,6]]
+# k=list(map(lambda x: x+[5],l))
+# print(k)
+#2
+# d={"apple":100,"banana":40,"cherry":15}
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# f=dict(filter(lambda x:x[1]>50,d.items()))
+# print(f)
+#3
+# k=input()
+# print(k.split("$"))
+# print(type(k))
+#10
+# from functools import reduce
+# l=[5,10,15,20,25,30]
+# sq=list(map(lambda x: x**2,l))
+# di=list(filter(lambda x: x%5==0,sq ))
+# v=reduce(lambda x,y:x+y,di)
+# print
+#4
+#k= reduce (lambda x,y: x+y [1,2,3])
+
+# 8
+# l=[10,350,10,20,350]
+# k= list(map(lambda x: id(x), l))
+# print(k)
+
